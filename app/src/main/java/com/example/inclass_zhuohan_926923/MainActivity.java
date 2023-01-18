1ab37e649166ea4028c75b0bef1df1be50b38b04
zhuohansong886 <ssszh886@gmail.com>
2023-01-18 16:16:32 -0500

    inClass_week2

---
+++ app/src/main/java/com/example/inclass_zhuohan_926923/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.inclass_zhuohan_926923;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPractice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        buttonPractice = findViewById(R.id.button);
+
+
+        buttonPractice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toPracticeActivity = new Intent(MainActivity.this,
+                        PracticeActivity.class);
+
+                startActivity(toPracticeActivity);
+            }
+        });
+    }
+}
